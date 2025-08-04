c89a304b94457ed8ef0e9568d6cf42766de6cebd
Hanishkar21 <hanishhanishkar5@gmail.com>
2025-08-04 21:52:47 +0530

    Add files via upload

---
+++ day2_python programming.py
@@ -0,0 +1,9 @@
+name=input("enter your name:")
+mark1=int(input("enter mark1:"))
+mark2=int(input("enter mark2:"))
+mark3=int(input("enter mark3:"))
+total=mark1+mark2+mark3
+average=total/3
+print("name:",name)
+print("total:",total)
+print("average:",average)
